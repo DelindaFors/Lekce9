17c6a871b5598d09629d5130fa920d6f38c27874
Daniela Tichá <daniela.ticha@post.cz>
2023-11-30 19:10:28 +0100

    2

---
+++ script.js
@@ -0,0 +1,22 @@
+/*const formatPriceCZK = (price) => {
+    return `${price} CZK`
+}
+
+const formatPriceEUR = (price) => {
+    return `${price} EUR`
+}
+
+const formatPriceUSD = (price) => {
+    return `${price} USD`
+}
+
+const renderProduct = (name, decription, price, formatPrice) => {
+    return `
+    <h2> ${name}</h2>
+    <p> ${description}</p>
+    <p> ${formatPrice(price)} </p>
+    `
+}
+
+document.body.innerHTML = renderProduct ("Pizza Salami", "Nejlepší pizza na světě", 200, formatPriceCZK)*/
+
