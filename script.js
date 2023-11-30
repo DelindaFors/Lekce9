20f9b9cc538b642445117fa2c96d7c7e1bfff715
Daniela Tichá <daniela.ticha@post.cz>
2023-11-30 20:35:33 +0100

    3

--- script.js
+++ script.js
@@ -20,3 +20,52 @@ const renderProduct = (name, decription, price, formatPrice) => {
 
 document.body.innerHTML = renderProduct ("Pizza Salami", "Nejlepší pizza na světě", 200, formatPriceCZK)*/
 
+/*const message = () => {
+    alert ("Haha, vypršel ti čas, opakuješ semestr :)")
+}
+
+setTimeout (message, 5000)*/
+
+/*setInterval(() => {
+    document.body.innerHTML += "<p>YOU HAVE BEEN HACKED HAHAHAHAHA</p>"
+}, 5000)*/
+
+/*let seconds = 0
+let minutes = 0
+
+/*odpočítavací hodiny
+setInterval(() => {
+    if(seconds === 60) {
+        seconds = 0
+        minutes++ // minutes = minutes + 1; minutes--
+    } else {
+        seconds++
+    }
+
+    document.body.innerHTML = `${String(minutes).padStart (2,"0")}:${String (seconds).padStart (2,"0")}`
+}, 1000)*/
+
+/*let btn = document.querySelector ("#test")
+
+btn. eddEventListener ("click", (e) => {
+    e.target
+}) funkce v parametru*/
+
+/*ukol objednavka*/
+
+/*const changeTitle = () => {
+    const odstavecElement = document.querySelector('#button-order');
+    odstavecElement.textContent = 'Objednáno';
+  };
+
+const buttonElement = document.querySelector('#button-order');
+buttonElement.addEventListener('click', changeTitle);*/
+
+const form = document.querySelector ("#form")
+
+form.addEventListener ("submit", (e) => {
+    const txtEl = document.querySelector ("elTxt")
+    e.preventDefault () /*zablokovaná následná funkce - neaktualizuje se stránko po odeslání formuláře*/
+    
+    alert (`Vítej uživateli, ${Number (txtEL.value)}!`)
+})
